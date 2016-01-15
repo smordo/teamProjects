387ad1988a730b6285391d08d33c30152069f460
Veronica Sykes <vasykes@gmail.com>
2016-01-15 13:09:55 -0800

    created cars controller

---
+++ controllers/cars_controller.js
@@ -0,0 +1,61 @@
+var Car = require('./models/car')
+
+
+function index(req,res){
+  Car.find(function(err,cars){
+    if(err) res.json({message: "No cars found"})
+    res.json(cars)
+  })
+}
+
+function show(req,res){
+  var id = request.params.id
+  Car.findById({_id: id}, function(err, car){
+    if (err) res.json({message: "No car found"})
+    res.json({car: car})
+  });
+}
+
+function create(req,res){
+  var car  = new Car()
+  car.make = req.body.make
+  car.model = req.body.model
+  car.year = req.body.year
+  car.color = req.body.color
+
+  car.save(function(err){
+    if(err) res.json({message:"Cannot create car"})
+    res.redirect('/cars')
+  })
+}
+
+function update(req,res) {
+  var id = request.params.id
+  Car.findOneAndUpdate({_id: id}, function(err, car){
+    if (err) res.json({message: "No car found"})
+    if(req.body.make) car.make = req.body.make
+    if(req.body.model) car.model = req.body.model
+    if(req.body.year) car.year = req.body.year
+    if(req.body.color) car.color = req.body.color
+
+    car.save(function(err){
+      if(err) res.json({message:"Cannot update car"})
+      res.json({message: "Car updated!"})
+  });
+}
+
+function destroy(req,res){
+  var id = request.params.id
+  Car.findById({_id: id}, function(err, car){
+    if (err) res.json({message: "No car found"})
+    res.json({message: "Car deleted."})
+  })
+}
+
+module.exports = {
+  index: index,
+  show: show,
+  create: create,
+  update: update,
+  destroy: destroy
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 1.) added node and server
 2.) added gitignore
 3.) created initial routes
+4.) deployed to heroku
+5.) created car model
